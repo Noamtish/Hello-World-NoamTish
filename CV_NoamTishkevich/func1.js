1aa1422f824a249b40c4ed2440b318a46eb8a98d
Noam <noamido7777@gmail.com>
2020-12-16 20:36:04 +0200

    modified

---
+++ CV_NoamTishkevich/func1.js
@@ -0,0 +1,13 @@
+
+
+
+  function img_increase()
+  {
+      document.getElementById("image").style.width="150px";
+      document.getElementById("image").style.height="120px";
+    } 
+
+  function img_decrease()
+  {
+     document.getElementById("image").style.width="100px";
+  }
