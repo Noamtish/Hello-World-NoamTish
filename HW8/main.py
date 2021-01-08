d0ad9fb93e2703d6cf5d3132cf5d76ab6810114e
Noam <noamido7777@gmail.com>
2021-01-08 17:42:33 +0200

    HW9 uploaded

--- HW8/main.py
+++ HW8/main.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template,redirect,url_for,send_file
+from flask import Flask, render_template,redirect,url_for,request,session
 
 app=Flask(__name__)
+app.secret_key= 'session123'
+
+
 @app.route('/')
+def Home():
+    return 'Home Page'
+
 @app.route('/page1')
-def home():
+def CV():
     return render_template('final_cv.html')
 
 @app.route('/Contact')
@@ -14,6 +20,43 @@ def change_loc():
 @app.route('/Assignment8')
 def A8():
     return render_template('Assignment8.html', name='Noam', dogs=['tokyo', 'shibel', 'rio'])
+
+
+@app.route('/Assignment9_logout',methods=['post','get'])
+def logout():
+    session['logged_in'] = False
+    username = ''
+    session['username']=username
+    return redirect(url_for('A9'))
+
+
+@app.route('/Assignment9',methods=['post','get'])
+def A9():
+  User_List={"noam": "student", "tomer": "mailmen", "sharon": "teacher", "amit": "Soccer player"}
+  name = 'empty'
+
+  #session['logged_in'] = False
+
+  if request.method == 'GET':
+
+    if 'name' in request.args:
+
+        name = request.args['name']
+
+  if request.method == 'POST':
+
+    username = request.form['username']
+    session['username'] = username
+    session['logged_in'] = True
+
+
+
+
+  return render_template('Assignment9.html', R_method=request.method, name=name, User_List=User_List, username=session['username'])
+
+
+
+
 
 @app.route('/temp2')
 def temp2():
